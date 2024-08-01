43bcfeedcb7f205724db3f5fb2bed7b5073d1048
pd5rm <pd5rm@users.noreply.github.com>
2024-08-01 12:19:20 -0700

    .

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "blog" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog_alb"
+  name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
