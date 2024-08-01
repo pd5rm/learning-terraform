a387085507f82d16cc22ff194c26c1330eb24d1e
pd5rm <pd5rm@users.noreply.github.com>
2024-08-01 12:47:03 -0700

    .

--- main.tf
+++ main.tf
@@ -35,20 +35,23 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.7.0"
   
-  subnet_id = module.blog_vpc.public_subnets[0]
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_id = modules.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns 
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
-
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
