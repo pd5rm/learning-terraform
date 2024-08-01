a5396e214828ce0bd22285702d60a6656f5c7ac7
pd5rm <pd5rm@users.noreply.github.com>
2024-08-01 12:49:32 -0700

    .

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_id = modules.blog_vpc.public_subnets
+  vpc_zone_identifier = modules.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns 
   security_groups = [module.blog_sg.security_group_id]
 
@@ -75,7 +75,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
