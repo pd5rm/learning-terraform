53c40ec22dcad082182443bf758945a837fc1a99
pd5rm <pd5rm@users.noreply.github.com>
2024-07-31 23:21:46 -0700

    .

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
   tags = {
     Name = "HelloWorld"
   }
@@ -39,7 +39,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port = 80
   to_port = 80
   protocol = "tcp"
-  cidr_blocks = "[0.0.0.0/0]"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port = "443"
   to_port = 443
   protocol = "tcp"
-  cidr_blocks = "[0.0.0.0/0]"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
